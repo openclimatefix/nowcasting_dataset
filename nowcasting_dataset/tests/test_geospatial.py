996f3ad955c881c382155c5665385e317495e1fc
Peter Dudfield <>
2021-08-10 14:59:03 +0100

    add option to update the transformations, but only need to load them once.

--- nowcasting_dataset/tests/test_geospatial.py
+++ nowcasting_dataset/tests/test_geospatial.py
@@ -17,8 +17,7 @@ def test_osgb_to_lat_lon():
     except:
         # Sometimes this test fails, then run the following code.
         # This forces a fresh 'grid' to be downloaded.
-        pyproj.transformer.\
-            TransformerGroup(crs_from=geospatial.OSGB, crs_to=geospatial.WGS84).download_grids(verbose=True)
+        geospatial.download_grids()
 
     finally:
         osgb_coords = geospatial.osgb_to_lat_lon(x=0, y=0)

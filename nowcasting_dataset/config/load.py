5144e0d7186219c609c402604bfe93b2363f759c
peterdudfield <peter.dudfield@hotmail.com>
2021-08-15 18:47:24 +0100

    add load method to gcs

--- nowcasting_dataset/config/load.py
+++ nowcasting_dataset/config/load.py
@@ -1,5 +1,11 @@
+import logging
+import gcsfs
+import os
+import io
 import yaml
-from .model import Configuration
+from nowcasting_dataset.config.model import Configuration
+
+logger = logging.getLogger(__name__)
 
 
 def load_yaml_configuration(filename: str) -> Configuration:
@@ -17,3 +23,37 @@ def load_yaml_configuration(filename: str) -> Configuration:
     configuration = Configuration(**configuration)
 
     return configuration
+
+
+def load_configuration_from_gcs(
+    gcp_dir: str, bucket: str = "solar-pv-nowcasting-data", filename: str = "configuration.yaml"
+) -> Configuration:
+    """
+    Load configuration from gcs
+
+    gcp_dir: the directory where the configruation is saved
+    bucket: the gcs bucket to load from
+    filename: the filename that will be loaded
+
+    Returns: configuration class
+    """
+
+    logger.info("Loading configuration from gcs")
+
+    bucket_and_dir = os.path.join(f"gs://{bucket}", gcp_dir)
+    filename = os.path.join(bucket_and_dir, filename)
+    logger.debug(f'Will be opening {filename}')
+
+    # set up gcs
+    gcs = gcsfs.GCSFileSystem(access="read_only")
+
+    # load the file into bytes
+    with gcs.open(filename, mode="rb") as file:
+        file_bytes = file.read()
+
+    # load the bytes to yaml
+    with io.BytesIO(file_bytes) as file:
+        data = yaml.load(file)
+
+    # put into pydantic class and returns
+    return Configuration(**data)

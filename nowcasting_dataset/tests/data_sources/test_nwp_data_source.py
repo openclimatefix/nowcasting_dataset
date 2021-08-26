7ad599e95d4865a57252083582cc902aa5cc33e6
peterdudfield <peter.dudfield@hotmail.com>
2021-08-26 15:34:39 +0100

    add test to load nwp batch

--- nowcasting_dataset/tests/data_sources/test_nwp_data_source.py
+++ nowcasting_dataset/tests/data_sources/test_nwp_data_source.py
@@ -35,3 +35,30 @@ def test_nwp_data_source_open():
     )
 
     nwp.open()
+
+def test_nwp_data_source_batch():
+
+    path = os.path.dirname(nowcasting_dataset.__file__)
+
+    # Solar PV data (test data)
+    NWP_FILENAME = f"{path}/tests/data/nwp_data/test.zarr"
+
+    nwp = NWPDataSource(
+        filename=NWP_FILENAME,
+        history_len=6,
+        forecast_len=12,
+        convert_to_numpy=True,
+        n_timesteps_per_batch=8,
+    )
+
+    nwp.open()
+
+    t0_datetimes = nwp._data.init_time[2:10].values
+    x = nwp._data.x[0:4].values
+    y = nwp._data.x[0:4].values
+
+    batch = nwp.get_batch(t0_datetimes=t0_datetimes,
+                  x_locations=x,
+                  y_locations=y)
+
+    assert len(batch) == 4

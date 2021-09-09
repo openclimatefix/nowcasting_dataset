f7f4f0f59673fc8a98d5e1c8391d59fa03ab43d1
peterdudfield <peter.dudfield@hotmail.com>
2021-09-09 09:58:06 +0100

    add data source constants

---
+++ nowcasting_dataset/data_sources/constants.py
@@ -0,0 +1,20 @@
+PV_SYSTEM_ID: str = 'pv_system_id'
+PV_SYSTEM_ROW_NUMBER = 'pv_system_row_number'
+PV_SYSTEM_X_COORDS = 'pv_system_x_coords'
+PV_SYSTEM_Y_COORDS = 'pv_system_y_coords'
+PV_AZIMUTH_ANGLE = 'pv_azimuth_angle'
+PV_ELEVATION_ANGLE = 'pv_elevation_angle'
+PV_YIELD = 'pv_yield'
+N_PV_SYSTEMS_PER_EXAMPLE = 128
+
+GSP_SYSTEM_ID: str = "gsp_system_id"
+GSP_YIELD = "gsp_yield"
+GSP_SYSTEM_X_COORDS = "gsp_system_x_coords"
+GSP_SYSTEM_Y_COORDS = "gsp_system_y_coords"
+GSP_DATETIME_INDEX = "gsp_datetime_index"
+N_GSP_PER_EXAMPLE = 32
+
+CENTROID_TYPE = "centroid_type"
+
+
+DATETIME_FEATURE_NAMES = ("hour_of_day_sin", "hour_of_day_cos", "day_of_year_sin", "day_of_year_cos")

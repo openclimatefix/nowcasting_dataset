c92da100222cff4344b6621f26c44219bfe3d263
peterdudfield <peter.dudfield@hotmail.com>
2021-09-16 18:01:46 +0100

    add day method

--- nowcasting_dataset/dataset/split.py
+++ nowcasting_dataset/dataset/split.py
@@ -1,6 +1,7 @@
 """ Function to split datasets up """
 
 import pandas as pd
+import numpy as np
 from typing import List, Tuple, Union
 
 import logging
@@ -31,7 +32,54 @@ def split(
         train_datetimes = datetimes
         validation_datetimes = datetimes
         test_datetimes = datetimes
+    elif method == "day":
+        train_datetimes, validation_datetimes, test_datetimes = _split_day(
+            datetimes=datetimes, train_test_validation_split=train_test_validation_split
+        )
     else:
         raise
 
     return train_datetimes, validation_datetimes, test_datetimes
+
+
+def _split_day(
+    datetimes: Union[List[pd.Timestamp], pd.DatetimeIndex],
+    train_test_validation_split: Tuple[int] = (3, 1, 1),
+):
+    """
+
+    Args:
+        datetimes:
+        train_test_validation_split:
+
+    Returns:
+
+    """
+
+    total_weights = sum(train_test_validation_split)
+    cum_weights = np.cumsum(train_test_validation_split)
+
+    # make which day indexes go i.e if the split is [3,1,1] then the
+    # - train_ indexes = [0,1,2]
+    # - validation_indexes = [3]
+    # - test_indexes = [4]
+    train_indexes = [i for i in range(total_weights) if i < cum_weights[0]]
+    validation_indexes = [
+        i for i in range(total_weights) if (i >= cum_weights[0]) & (i < cum_weights[1])
+    ]
+    test_indexes = [i for i in range(total_weights) if (i >= cum_weights[1]) & (i < cum_weights[2])]
+
+    # select the datetimes
+    train = [
+        datetime for datetime in datetimes if datetime.dayofyear % total_weights in train_indexes
+    ]
+    validation = [
+        datetime
+        for datetime in datetimes
+        if datetime.dayofyear % total_weights in validation_indexes
+    ]
+    test = [
+        datetime for datetime in datetimes if datetime.dayofyear % total_weights in test_indexes
+    ]
+
+    return train, validation, test
--- tests/dataset/test_split.py
+++ tests/dataset/test_split.py
@@ -11,3 +11,17 @@ def test_split_same():
     assert (train == datetimes).all()
     assert (validation == datetimes).all()
     assert (test == datetimes).all()
+
+
+def test_split_day():
+
+    datetimes = pd.date_range("2021-01-01", "2021-02-01", freq="5T")
+
+    train, validation, test = split(datetimes=datetimes, method="day")
+
+    for d in train:
+        assert d.dayofyear % 5 in [0, 1, 2]
+    for d in validation:
+        assert d.dayofyear % 5 in [3]
+    for d in test:
+        assert d.dayofyear % 5 in [4]

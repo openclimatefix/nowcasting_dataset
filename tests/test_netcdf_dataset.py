20eed11cd4e916964fe92d27d22e73e1f349efb1
Jacob Bieker <jacob@openclimatefix.org>
2021-09-16 14:09:51 +0100

    Import more constants

--- tests/test_netcdf_dataset.py
+++ tests/test_netcdf_dataset.py
@@ -8,6 +8,10 @@ from nowcasting_dataset.consts import (
     NWP_DATA,
     SATELLITE_DATETIME_INDEX,
     NWP_TARGET_TIME,
+    NWP_Y_COORDS,
+    NWP_X_COORDS,
+    PV_YIELD,
+    GSP_YIELD,
 )
 import plotly.graph_objects as go
 import plotly
@@ -23,7 +27,7 @@ def test_subselect_date():
     seq_length = 20
     x = {
         SATELLITE_DATA: np.random.random((batch_size, seq_length, 128, 128, 12)),
-        "pv_yield": np.random.random((batch_size, seq_length, 128)),
+        PV_YIELD: np.random.random((batch_size, seq_length, 128)),
         "pv_system_id": np.random.random((batch_size, 128)),
         NWP_DATA: np.random.random((batch_size, 10, seq_length, 2, 2)),
         "hour_of_day_sin": np.random.random((batch_size, seq_length)),
@@ -33,7 +37,7 @@ def test_subselect_date():
     }
 
     # add a nan
-    x["pv_yield"][0, 0, :] = float("nan")
+    x[PV_YIELD][0, 0, :] = float("nan")
 
     # add fake x and y coords, and make sure they are sorted
     x[SATELLITE_X_COORDS] = np.random.random((batch_size, seq_length))
@@ -53,8 +57,8 @@ def test_subselect_date():
         history_minutes=10,
         forecast_minutes=10,
     )
-    assert batch["sat_data"].shape[1] == 5
-    assert batch["nwp"].shape[2] == 5
+    assert batch[SATELLITE_DATA].shape[1] == 5
+    assert batch[NWP_DATA].shape[2] == 5
 
 
 @pytest.mark.skip("CD does not have access to GCS")
@@ -84,10 +88,10 @@ def test_get_dataloaders_gcp():
     data = next(t)
 
     # image
-    z = data["sat_data"][0][0][:, :, 0]
-    _ = data["gsp_yield"][0][:, 0]
+    z = data[SATELLITE_DATA][0][0][:, :, 0]
+    _ = data[GSP_YIELD][0][:, 0]
 
-    _ = pd.to_datetime(data["sat_datetime_index"][0, 0], unit="s")
+    _ = pd.to_datetime(data[SATELLITE_DATETIME_INDEX][0, 0], unit="s")
 
     fig = go.Figure(data=go.Contour(z=z))
 
@@ -124,7 +128,7 @@ def test_get_dataloaders_aws():
         t = iter(train_dataset)
         data = next(t)
 
-        assert "sat_data" in data.keys()
+        assert SATELLITE_DATA in data.keys()
 
 
 @pytest.mark.skip("CD does not have access to GCP")
@@ -142,12 +146,12 @@ def test_required_keys_gcp():
         os.path.join(TEMP_PATH, "train"),
         cloud="gcp",
         required_keys=[
-            "nwp",
-            "nwp_x_coords",
-            "nwp_y_coords",
-            "sat_data",
-            "sat_x_coords",
-            "sat_y_coords",
+            NWP_DATA,
+            NWP_X_COORDS,
+            NWP_Y_COORDS,
+            SATELLITE_DATA,
+            SATELLITE_X_COORDS,
+            SATELLITE_Y_COORDS,
         ],
     )
 
@@ -168,8 +172,8 @@ def test_required_keys_gcp():
     t = iter(train_dataset)
     data = next(t)
 
-    assert "sat_data" in data.keys()
-    assert "pv_yield" not in data.keys()
+    assert SATELLITE_DATA in data.keys()
+    assert PV_YIELD not in data.keys()
 
 
 @pytest.mark.skip("CD does not have access to GCP")
@@ -208,8 +212,8 @@ def test_subsetting_gcp():
     t = iter(train_dataset)
     data = next(t)
 
-    sat_data = data["sat_data"]
+    sat_data = data[SATELLITE_DATA]
 
     # Sat is in 5min increments, so should have 2 history + current + 2 future
     assert sat_data.shape[1] == 5
-    assert data["nwp"].shape[2] == 5
+    assert data[NWP_DATA].shape[2] == 5

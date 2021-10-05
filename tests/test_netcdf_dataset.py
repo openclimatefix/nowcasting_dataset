c0df51deaddb5383843db800dba525b34752c55c
peterdudfield <peter.dudfield@hotmail.com>
2021-10-05 17:33:32 +0100

    re introduce tests

--- tests/test_netcdf_dataset.py
+++ tests/test_netcdf_dataset.py
@@ -49,72 +49,49 @@ def test_subselect_date(test_data_folder):
     assert batch.nwp.nwp.shape[2] == 5
 
 
-#
-# TODO need to un commit these
-# def test_subselect_date_with_t0_dt(test_data_folder):
-#     dataset = xr.open_dataset(f"{test_data_folder}/0.nc")
-#     x = example.Example(
-#         sat_data=dataset["sat_data"],
-#         nwp=dataset["nwp"],
-#         nwp_target_time=dataset["nwp_time_coords"],
-#         sat_datetime_index=dataset["sat_time_coords"],
-#     )
-#     x[T0_DT] = x[SATELLITE_DATETIME_INDEX].isel(time=7)
-#
-#     batch = subselect_data(
-#         x,
-#         required_keys=(NWP_DATA, NWP_TARGET_TIME, SATELLITE_DATA, SATELLITE_DATETIME_INDEX),
-#         history_minutes=10,
-#         forecast_minutes=10,
-#     )
-#
-#     assert batch[SATELLITE_DATA].shape[1] == 5
-#     assert batch[NWP_DATA].shape[2] == 5
+def test_netcdf_dataset_local_using_configuration(configuration: Configuration):
+    DATA_PATH = os.path.join(os.path.dirname(nowcasting_dataset.__file__), "../tests", "data")
+    TEMP_PATH = os.path.join(
+        os.path.dirname(nowcasting_dataset.__file__), "../tests", "data", "temp"
+    )
 
+    train_dataset = NetCDFDataset(
+        1,
+        DATA_PATH,
+        TEMP_PATH,
+        cloud="local",
+        history_minutes=10,
+        forecast_minutes=10,
+        required_keys=[NWP_DATA, NWP_TARGET_TIME, SATELLITE_DATA, SATELLITE_DATETIME_INDEX],
+        configuration=configuration,
+    )
 
-# def test_netcdf_dataset_local_using_configuration(configuration: Configuration):
-#     DATA_PATH = os.path.join(os.path.dirname(nowcasting_dataset.__file__), "../tests", "data")
-#     TEMP_PATH = os.path.join(
-#         os.path.dirname(nowcasting_dataset.__file__), "../tests", "data", "temp"
-#     )
-#
-#     train_dataset = NetCDFDataset(
-#         1,
-#         DATA_PATH,
-#         TEMP_PATH,
-#         cloud="local",
-#         history_minutes=10,
-#         forecast_minutes=10,
-#         required_keys=[NWP_DATA, NWP_TARGET_TIME, SATELLITE_DATA, SATELLITE_DATETIME_INDEX],
-#         configuration=configuration,
-#     )
-#
-#     dataloader_config = dict(
-#         pin_memory=True,
-#         num_workers=1,
-#         prefetch_factor=1,
-#         worker_init_fn=worker_init_fn,
-#         persistent_workers=True,
-#         # Disable automatic batching because dataset
-#         # returns complete batches.
-#         batch_size=None,
-#     )
-#
-#     _ = torch.utils.data.DataLoader(train_dataset, **dataloader_config)
-#
-#     train_dataset.per_worker_init(1)
-#     t = iter(train_dataset)
-#     data = next(t)
-#
-#     sat_data = data[SATELLITE_DATA]
-#
-#     # TODO
-#     # Sat is in 5min increments, so should have 2 history + current + 2 future
-#     assert sat_data.shape[1] == 5
-#     assert data[NWP_DATA].shape[2] == 5
-#
-#     # Make sure file isn't deleted!
-#     assert os.path.exists(os.path.join(DATA_PATH, "0.nc"))
+    dataloader_config = dict(
+        pin_memory=True,
+        num_workers=1,
+        prefetch_factor=1,
+        worker_init_fn=worker_init_fn,
+        persistent_workers=True,
+        # Disable automatic batching because dataset
+        # returns complete batches.
+        batch_size=None,
+    )
+
+    _ = torch.utils.data.DataLoader(train_dataset, **dataloader_config)
+
+    train_dataset.per_worker_init(1)
+    t = iter(train_dataset)
+    data = next(t)
+
+    sat_data = data.satellite.sat_data
+
+    # TODO
+    # Sat is in 5min increments, so should have 2 history + current + 2 future
+    assert sat_data.shape[1] == 5
+    assert data.nwp.nwp.shape[2] == 5
+
+    # Make sure file isn't deleted!
+    assert os.path.exists(os.path.join(DATA_PATH, "0.nc"))
 
 
 @pytest.mark.skip("CD does not have access to GCS")
@@ -148,9 +125,9 @@ def test_get_dataloaders_gcp(configuration: Configuration):
 
     # image
     z = data.satellite.sat_data[0][0][:, :, 0]
-    _ = data[GSP_YIELD][0][:, 0]
+    _ = data.gsp.gsp_yield[0][:, 0]
 
-    _ = pd.to_datetime(data[SATELLITE_DATETIME_INDEX][0, 0], unit="s")
+    _ = pd.to_datetime(data.satellite.sat_datetime_index[0, 0], unit="s")
 
     fig = go.Figure(data=go.Contour(z=z))
 

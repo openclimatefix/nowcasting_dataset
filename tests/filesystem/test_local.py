298f5857dbab51d26c489c5da3ef56892f5485bd
peterdudfield <peter.dudfield@hotmail.com>
2021-10-08 17:07:13 +0100

    ad tests for download and upload

--- tests/filesystem/test_local.py
+++ tests/filesystem/test_local.py
@@ -1,7 +1,13 @@
 import os
 import tempfile
 from pathlib import Path
-from nowcasting_dataset.filesystem.utils import delete_all_files_in_temp_path, check_path_exists
+from nowcasting_dataset.filesystem.utils import (
+    delete_all_files_in_temp_path,
+    check_path_exists,
+    download_to_local,
+    get_all_filenames_in_path,
+    upload_one_file,
+)
 
 
 def test_check_file_exists():
@@ -52,3 +58,61 @@ def test_delete_local_files():
         # check the object are not there
         assert not os.path.exists(path_and_filename_1)
         assert not os.path.exists(path_and_filename_2)
+
+
+def test_download():
+
+    file1 = "test_file1.txt"
+    file2 = "test_dir/test_file2.txt"
+    file3 = "test_file3.txt"
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        local_path = Path(tmpdirname)
+
+        # add fake file to dir
+        path_and_filename_1 = os.path.join(local_path, file1)
+        with open(path_and_filename_1, "w"):
+            pass
+
+        # add fake file to dir
+        os.mkdir(f"{tmpdirname}/test_dir")
+        path_and_filename_2 = os.path.join(local_path, file2)
+        with open(os.path.join(local_path, file2), "w"):
+            pass
+
+        # run function
+        path_and_filename_3 = os.path.join(local_path, file3)
+        download_to_local(remote_filename=path_and_filename_1, local_filename=path_and_filename_3)
+
+        # check the object are not there
+        filenames = get_all_filenames_in_path(local_path)
+        assert len(filenames) == 3
+
+
+def test_upload():
+
+    file1 = "test_file1.txt"
+    file2 = "test_dir/test_file2.txt"
+    file3 = "test_file3.txt"
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        local_path = Path(tmpdirname)
+
+        # add fake file to dir
+        path_and_filename_1 = os.path.join(local_path, file1)
+        with open(path_and_filename_1, "w"):
+            pass
+
+        # add fake file to dir
+        os.mkdir(f"{tmpdirname}/test_dir")
+        path_and_filename_2 = os.path.join(local_path, file2)
+        with open(os.path.join(local_path, file2), "w"):
+            pass
+
+        # run function
+        path_and_filename_3 = os.path.join(local_path, file3)
+        upload_one_file(remote_filename=path_and_filename_3, local_filename=path_and_filename_1)
+
+        # check the object are not there
+        filenames = get_all_filenames_in_path(local_path)
+        assert len(filenames) == 3

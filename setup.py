d83a88aab54e176b66798494c9dd437e11502f3f
peterdudfield <peter.dudfield@hotmail.com>
2021-10-20 14:59:30 +0100

    use 'nowcasting_dataset/config/*.yaml' full path

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     extras_require={"torch": ["torch", "pytorch_lightning"]},
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_data={"config": ["*.yaml"]},
+    package_data={"config": ["nowcasting_dataset/config/*.yaml"]},
+    include_package_data=True,
     packages=find_packages(),
 )

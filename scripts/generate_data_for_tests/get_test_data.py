11eeca1d5909ec0dbbc443892917e2e2d81f5e02
Jacob Bieker <jacob@openclimatefix.org>
2021-10-20 13:41:02 +0100

    Update get_test_data.py

--- scripts/generate_data_for_tests/get_test_data.py
+++ scripts/generate_data_for_tests/get_test_data.py
@@ -96,6 +96,27 @@ encoding = {
 
 gsp_power.to_zarr(f"{local_path}/tests/data/gsp/test.zarr", mode="w", encoding=encoding)
 
+# ### satellite
+
+# s = SatelliteDataSource(filename="gs://solar-pv-nowcasting-data/satellite/EUMETSAT/SEVIRI_RSS/OSGB36/"
+#                                  "all_zarr_int16_single_timestep.zarr",
+#                           history_length=6,
+#                           forecast_length=12,
+#                           convert_to_numpy=True,
+#                           image_size_pixels=64,
+#                           meters_per_pixel=2000,
+#                           n_timesteps_per_batch=32)
+#
+# s.open()
+# start_dt = datetime.fromisoformat("2019-01-01 00:00:00.000+00:00")
+# end_dt = datetime.fromisoformat("2019-01-02 00:00:00.000+00:00")
+#
+# data_xarray = s._data
+# data_xarray = data_xarray.sel(time=slice(start_dt, end_dt))
+# data_xarray = data_xarray.sel(variable=['HRV'])
+# data_xarray = data_xarray.sel(x=slice(122000, 122001))
+#
+# data_df = data_xarray.to_dataframe()
 # TODO reduce and save
 
 

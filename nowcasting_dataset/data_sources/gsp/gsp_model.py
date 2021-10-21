40e9960751bf4b9f330dd12cfca4eb85b4623272
peterdudfield <peter.dudfield@hotmail.com>
2021-10-21 22:47:03 +0100

    make gsp model check simplier

--- nowcasting_dataset/data_sources/gsp/gsp_model.py
+++ nowcasting_dataset/data_sources/gsp/gsp_model.py
@@ -21,6 +21,6 @@ class GSP(DataSourceOutput):
         """ Check that all values are non NaNs """
         assert (~np.isnan(v.data)).all(), f"Some gsp data values are NaNs"
         assert (v.data != np.Inf).all(), f"Some gsp data values are Infinite"
-        assert (v.data >= -1e-7).all(), f"Some gsp data values are below 0 {v.data.min()}"
+        assert (v.data >= 0).all(), f"Some gsp data values are below 0 {v.data.min()}"
 
         return v

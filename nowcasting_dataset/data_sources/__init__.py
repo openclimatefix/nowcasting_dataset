0d47d0ac32d4e1ceda51cb321c8a91b1988933be
Jacob Bieker <jacob@bieker.tech>
2021-11-01 16:17:20 +0000

    Add to init

--- nowcasting_dataset/data_sources/__init__.py
+++ nowcasting_dataset/data_sources/__init__.py
@@ -1,6 +1,7 @@
 """ Various DataSources """
 from nowcasting_dataset.data_sources.data_source import DataSource  # noqa: F401
 from nowcasting_dataset.data_sources.gsp.gsp_data_source import GSPDataSource
+from nowcasting_dataset.data_sources.optical_flow.optical_flow_data_source import OpticalFlowDataSource
 from nowcasting_dataset.data_sources.nwp.nwp_data_source import NWPDataSource
 from nowcasting_dataset.data_sources.pv.pv_data_source import PVDataSource
 from nowcasting_dataset.data_sources.satellite.satellite_data_source import SatelliteDataSource

762ef7fc3214c7226cdf3f56ce99ebd901297ec3
peterdudfield <peter.dudfield@hotmail.com>
2021-11-05 08:56:02 +0000

    add nighttime test back in

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -2,8 +2,12 @@
 from datetime import datetime
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
+
 import nowcasting_dataset
 from nowcasting_dataset.data_sources.gsp.gsp_data_source import GSPDataSource
+from nowcasting_dataset.data_sources.satellite.satellite_data_source import SatelliteDataSource
 from nowcasting_dataset.manager import Manager
 
 
@@ -42,6 +46,34 @@ def test_load_yaml_configuration():  # noqa: D103
     manager.initialise_data_sources()
     assert len(manager.data_sources) == 6
     assert isinstance(manager.data_source_which_defines_geospatial_locations, GSPDataSource)
+
+
+def test_get_daylight_datetime_index():
+    """ Check that 'manager' gets the correct t0 datetime over nighttime"""
+    filename = Path(nowcasting_dataset.__file__).parent.parent / "tests" / "data" / "sat_data.zarr"
+
+    sat = SatelliteDataSource(
+        zarr_path=filename,
+        history_minutes=30,
+        forecast_minutes=60,
+        image_size_pixels=64,
+        meters_per_pixel=2000,
+    )
+
+    manager = Manager()
+    manager.data_sources = {"sat": sat}
+    manager.data_source_which_defines_geospatial_locations = sat
+    t0_datetimes = manager.get_t0_datetimes_across_all_data_sources(freq="5T")
+
+    # The testing sat_data.zarr has contiguous data from 12:05 to 18:00.
+    # nowcasting_datamodule.history_minutes = 30
+    # nowcasting_datamodule.forecast_minutes = 60
+    # Daylight ends at 16:20.
+    # So the expected t0_datetimes start at 12:35 (12:05 + 30 minutes)
+    # and end at 15:20 (16:20 - 60 minutes)
+
+    correct_t0_datetimes = pd.date_range("2019-01-01 12:35", "2019-01-01 15:20", freq="5 min")
+    np.testing.assert_array_equal(t0_datetimes, correct_t0_datetimes)
 
 
 # TODO: Issue #322: Test the other Manager methods!

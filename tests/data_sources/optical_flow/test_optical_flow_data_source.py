8c03f10c285da2031171019d7f3c5854bc45fd0e
Jacob Bieker <jacob@bieker.tech>
2021-11-10 13:59:31 +0000

    Fix outside temp directory

--- tests/data_sources/optical_flow/test_optical_flow_data_source.py
+++ tests/data_sources/optical_flow/test_optical_flow_data_source.py
@@ -25,5 +25,5 @@ def test_optical_flow_data_source_get_batch(configuration):  # noqa: D103
     with tempfile.TemporaryDirectory() as dirpath:
         Batch.fake(configuration=configuration).save_netcdf(path=dirpath, batch_i=0)
 
-    optical_flow = optical_flow_datasource.get_batch(netcdf_path=dirpath, batch_idx=0)
-    print(optical_flow)
+        optical_flow = optical_flow_datasource.get_batch(netcdf_path=dirpath, batch_idx=0)
+        print(optical_flow)

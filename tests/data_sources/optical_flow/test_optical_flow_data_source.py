01e637623fe7ae951871aebed3d363632471e5c0
Jacob Bieker <jacob@bieker.tech>
2021-11-10 15:17:56 +0000

    Add assert

--- tests/data_sources/optical_flow/test_optical_flow_data_source.py
+++ tests/data_sources/optical_flow/test_optical_flow_data_source.py
@@ -31,8 +31,9 @@ def test_optical_flow_get_example(optical_flow_configuration):
 
 def test_optical_flow_data_source_get_batch(optical_flow_configuration):  # noqa: D103
     optical_flow_datasource = OpticalFlowDataSource(
-        previous_timestep_for_flow=1, opticalflow_image_size_pixels=64
+        previous_timestep_for_flow=1, opticalflow_image_size_pixels=32
     )
     with tempfile.TemporaryDirectory() as dirpath:
         Batch.fake(configuration=optical_flow_configuration).save_netcdf(path=dirpath, batch_i=0)
         optical_flow = optical_flow_datasource.get_batch(netcdf_path=dirpath, batch_idx=0)
+        assert optical_flow.values.shape == (4, 12, 32, 32, 12)

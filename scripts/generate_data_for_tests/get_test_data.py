e0a8d2201a78752b255f89255191d3cfb267d93d
Jacob Bieker <jacob@bieker.tech>
2021-11-17 10:21:12 +0000

    Fix test generation script

--- scripts/generate_data_for_tests/get_test_data.py
+++ scripts/generate_data_for_tests/get_test_data.py
@@ -85,25 +85,8 @@ nwp_data.to_zarr(f"{local_path}/tests/data/nwp_data/test.zarr")
 ####
 # ### GSP data
 #####
-import io
-import os
-import time
-from datetime import datetime
-from pathlib import Path
 
-import gcsfs
-import numcodecs
-import numpy as np
-import pandas as pd
-import xarray as xr
-
-import nowcasting_dataset
-from nowcasting_dataset.data_sources.nwp.nwp_data_source import NWP_VARIABLE_NAMES, open_nwp
-
-local_path = os.path.dirname(nowcasting_dataset.__file__) + "/.."
 filename = "/home/jacob/nowcasting_dataset/scripts/pv_gsp.zarr"
-start_dt = datetime.fromisoformat("2020-04-01 00:00:00.000+00:00")
-end_dt = datetime.fromisoformat("2020-04-02 00:00:00.000+00:00")
 gsp_power = xr.open_dataset(filename, engine="zarr")
 gsp_power = gsp_power.sel(datetime_gmt=slice(start_dt, end_dt))
 gsp_power = gsp_power.sel(gsp_id=slice(gsp_power.gsp_id[0], gsp_power.gsp_id[20]))

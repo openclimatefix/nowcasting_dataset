4c2c82fa7837c045076fd6690e60d3c370c083b9
BumpVersion Action <bumpversion@github-actions>
2021-12-02 11:05:37 +0000

    Bump version: 3.1.3 → 3.1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="nowcasting_dataset",
-    version="3.1.3",
+    version="3.1.4",
     license="MIT",
     description="Nowcasting Dataset",
     author="Jack Kelly, Peter Dudfield, Jacob Bieker",

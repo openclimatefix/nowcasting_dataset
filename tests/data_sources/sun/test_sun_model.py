bf34870f244bb479e5d9c00edb570b59072a887f
peterdudfield <peter.dudfield@hotmail.com>
2022-05-10 20:41:34 +0100

    fix sun tests

--- tests/data_sources/sun/test_sun_model.py
+++ tests/data_sources/sun/test_sun_model.py
@@ -9,12 +9,14 @@ from nowcasting_dataset.data_sources.fake.batch import sun_fake
 from nowcasting_dataset.data_sources.sun.sun_model import Sun
 
 
-def test_sun_init():  # noqa: D103
-    _ = sun_fake(batch_size=4, seq_length_5=17)
+def test_sun_init(configuration):  # noqa: D103
+    configuration.process.batch_size = 4
+    _ = sun_fake(configuration=configuration)
 
 
-def test_sun_validation():  # noqa: D103
-    sun = sun_fake(batch_size=4, seq_length_5=17)
+def test_sun_validation(configuration):  # noqa: D103
+    configuration.process.batch_size = 4
+    sun = sun_fake(configuration=configuration)
 
     Sun.model_validation(sun)
 
@@ -23,8 +25,9 @@ def test_sun_validation():  # noqa: D103
         Sun.model_validation(sun)
 
 
-def test_sun_validation_elevation():  # noqa: D103
-    sun = sun_fake(batch_size=4, seq_length_5=17)
+def test_sun_validation_elevation(configuration):  # noqa: D103
+    configuration.process.batch_size = 4
+    sun = sun_fake(configuration=configuration)
 
     Sun.model_validation(sun)
 
@@ -33,8 +36,9 @@ def test_sun_validation_elevation():  # noqa: D103
         Sun.model_validation(sun)
 
 
-def test_sun_validation_azimuth():  # noqa: D103
-    sun = sun_fake(batch_size=4, seq_length_5=17)
+def test_sun_validation_azimuth(configuration):  # noqa: D103
+    configuration.process.batch_size = 4
+    sun = sun_fake(configuration=configuration)
 
     Sun.model_validation(sun)
 
@@ -43,9 +47,11 @@ def test_sun_validation_azimuth():  # noqa: D103
         Sun.model_validation(sun)
 
 
-def test_sun_save():  # noqa: D103
+def test_sun_save(configuration):  # noqa: D103
+
+    configuration.process.batch_size = 4
     with tempfile.TemporaryDirectory() as dirpath:
-        sun = sun_fake(batch_size=4, seq_length_5=17)
+        sun = sun_fake(configuration=configuration)
         sun.save_netcdf(path=dirpath, batch_i=0)
 
         assert os.path.exists(f"{dirpath}/sun/000000.nc")

8c0dedfae5c3c8761b13e44c4f25a0a3d4dd90ea
peterdudfield <peter.dudfield@hotmail.com>
2022-05-25 11:12:49 +0100

    add test

--- tests/data_sources/sun/test_sun_data_source.py
+++ tests/data_sources/sun/test_sun_data_source.py
@@ -43,3 +43,21 @@ def test_get_example_different_year(test_data_folder):  # noqa 103
 
     assert len(example.elevation) == 19
     assert len(example.azimuth) == 19
+
+
+def test_get_load_live():  # noqa 103
+
+    sun_data_source = SunDataSource(
+        zarr_path="", history_minutes=30, forecast_minutes=60, load_live=True
+    )
+
+    x = 256895.63164759654
+    y = 666180.3018829626
+    start_dt = pd.Timestamp("2021-04-01 12:00:00.000")
+
+    example = sun_data_source.get_example(
+        location=SpaceTimeLocation(t0_datetime_utc=start_dt, x_center_osgb=x, y_center_osgb=y)
+    )
+
+    assert len(example.elevation) == 19
+    assert len(example.azimuth) == 19

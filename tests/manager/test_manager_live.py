957133e42ca3293296b4c9875bc12848d59e0c14
peterdudfield <peter.dudfield@hotmail.com>
2022-05-31 22:46:20 +0100

    fix

--- tests/manager/test_manager_live.py
+++ tests/manager/test_manager_live.py
@@ -218,7 +218,7 @@ def test_run_just_gsp(test_configuration_filename, gsp_yields_and_systems):
             gsp = xr.load_dataset(f"{local_temp_path_save}/gsp/000000.nc", engine="h5netcdf")
             gsp = GSP(gsp)
             assert len(gsp.time.values[0]) == 5
-            assert gsp.time.values[0, -1].to_datetime() == floor_minutes_dt(datetime.utcnow())
+            assert pd.to_datetime(gsp.time.values[0, -1]) == floor_minutes_dt(datetime.utcnow())
 
 
 def test_run(test_configuration_filename, gsp_yields_and_systems):
